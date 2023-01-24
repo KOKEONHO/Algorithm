f7c6f237ce205d9e8c6c342c62d7de3279426848
KOKEONHO <rhrjsgh97@gmail.com>
2023-01-24 12:31:34 +0900

    Refactor: "main 함수 생성"
    실행 테스트를 위한 main 함수 생성

--- src/programmers/level_2/StockPrice.java
+++ src/programmers/level_2/StockPrice.java
@@ -1,7 +1,27 @@
 package programmers.level_2;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
 class StockPrice {
-    public int[] solution(int[] prices) {
+    public static void main(String[] args) throws IOException {
+
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] prices = {1, 2, 3, 2, 3};
+
+        int[] result = solution(prices);
+
+        bw.write(Arrays.toString(result));
+        bw.newLine();
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static int[] solution(int[] prices) {
         int count;
         int[] answer = new int[prices.length];
         for (int i = 0; i < prices.length; i++) {
