409f60074d36db9af6a08155e7c34fca4ab9cfce
KOKEONHO <rhrjsgh97@gmail.com>
2023-02-06 14:52:36 +0900

    Refactor: "Baekjoon2644_AdjacncyMatrix 파일 이름 수정"
    백준 2644번 파일 이름 수정

--- src/baekjoon/silver/level_2/Baekjoon1260_AdjacencyMatrix.java
+++ src/baekjoon/silver/level_2/Baekjoon1260_AdjacencyMatrix.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-public class Baekjoon1260 {
+public class Baekjoon1260_AdjacencyMatrix {
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
