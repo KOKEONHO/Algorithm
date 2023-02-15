84efb5c18004a1108a6f9a37b54bcb0d732c7a21
KOKEONHO <rhrjsgh97@gmail.com>
2023-02-16 00:09:19 +0900

    Feat: "HackerRank DiagonalDifference 해결"
    HackerRank DiagonalDifference 문제 해결

---
+++ src/hackerrank/easy/DiagonalDifference.java
@@ -0,0 +1,61 @@
+package hackerrank.easy;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static int diagonalDifference(List<List<Integer>> arr) {
+
+        int primaryDiagonal = 0, secondaryDiagonal = 0;
+
+        for (int i = 0; i < arr.size(); i++) {
+            primaryDiagonal += arr.get(i).get(i);
+            secondaryDiagonal += arr.get(i).get((arr.size() - 1) - i);
+        }
+
+        return Math.abs(primaryDiagonal-secondaryDiagonal);
+
+    }
+
+}
+
+public class DiagonalDifference {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
