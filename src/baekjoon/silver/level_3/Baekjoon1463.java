664acea6b299430aee61e068f5a3f760536ec6ab
KOKEONHO <rhrjsgh97@gmail.com>
2023-02-28 02:24:37 +0900

    Feat: "Baekjoon1463 해결"
    백준 1463번 문제 해결

---
+++ src/baekjoon/silver/level_3/Baekjoon1463.java
@@ -0,0 +1,50 @@
+package baekjoon.silver.level_3;
+
+import java.io.*;
+
+public class Baekjoon1463 {
+
+    private static long[] array;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int X = Integer.parseInt(br.readLine());
+
+        array = new long[X + 1];
+
+        long result = dynamicProgramming(X);
+
+        bw.write(Long.toString(result));
+        bw.newLine();
+
+        bw.flush();
+        bw.close();
+    }
+
+    private static long dynamicProgramming(int X) {
+
+        array[1] = 0;
+        if (array.length > 2) {
+            for (int i = 2; i < array.length; i++) {
+                if (i % 2 == 0 || i % 3 == 0) {
+                    if (i % 2 == 0) {
+                        array[i] = Math.min(array[i / 2], array[i - 1]) + 1;
+                    }
+                    if (i % 3 == 0) {
+                        array[i] = Math.min(array[i / 3], array[i - 1]) + 1;
+                    }
+                    if (i % 2 == 0 && i % 3 == 0) {
+                        array[i] = Math.min(Math.min(array[i / 2], array[i / 3]), array[i - 1]) + 1;
+                    }
+                } else {
+                    array[i] = array[i - 1] + 1;
+                }
+            }
+        }
+
+        return array[X];
+    }
+}
